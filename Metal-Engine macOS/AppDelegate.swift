cd800fdfbc9ba9ababe980c9a2c3830d445f6e1d
Jeremiah Korreck <korreckj328@gmail.com>
2021-10-26 12:40:17 -0400

    Initial Commit

---
+++ Metal-Engine macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  Metal-Engine macOS
+//
+//  Created by Jeremiah Korreck on 10/26/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
